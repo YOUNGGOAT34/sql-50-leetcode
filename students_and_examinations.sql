125e938baf99f0cb6eb0eacb411f3cb9292a5820
GOAT <lucasonyiego283@gmail.com>
2026-03-04 15:52:25 +0300

    students and examinations

---
+++ students_and_examinations.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select Students.student_id,Students.student_name,Subjects.subject_name,count(Examinations.subject_name) as attended_exams
+from Students 
+cross join Subjects 
+left join Examinations on Students.student_id=Examinations.student_id and Subjects.subject_name=Examinations.subject_name
+Group by 1,2,3
+order by 1,3
+
