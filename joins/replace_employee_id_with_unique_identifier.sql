9ffb608b0aaa938db2287e82989152510adc1654
GOAT <lucasonyiego283@gmail.com>
2026-03-07 16:47:41 +0300

    restructuring

--- joins/replace_employee_id_with_unique_identifier.sql
+++ joins/replace_employee_id_with_unique_identifier.sql
@@ -5,5 +5,5 @@ FROM Employees
 
 LEFT JOIN EmployeeUNI ON Employees.id=EmployeeUNI.id
 
-# a left join includes everything from the left table ie employees in this case
+left join includes everything from the left table ie employees in this case
 
